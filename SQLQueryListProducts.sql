21dfb565e91596dd331630e6fa04f7942bcc110c
HasanovIV <103428531+HasanovIV@users.noreply.github.com>
2022-05-24 16:12:36 +0500

    Update SQLQueryListProducts.sql

--- SQLQueryListProducts.sql
+++ SQLQueryListProducts.sql
@@ -1,6 +1,6 @@
 Use AdventureWorks2012
 
-SELECT ProductID, Product.Name AS [Name product],ProductCategoryID, ProductCategory.Name AS [Name category]
+SELECT Product.Name AS [Name product], ProductCategory.Name AS [Name category]
 FROM Production.Product AS Product
 LEFT JOIN Production.ProductCategory AS ProductCategory
 ON Product.ProductSubcategoryID = ProductCategory.ProductCategoryID
